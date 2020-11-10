581e8a5130ef29b017eeffa0eac2519c34c92b1c
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-11-10 20:12:18 -0300

    feat: routes and styled-components

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </BrowserRouter>
+);
+
+export default Routes;
