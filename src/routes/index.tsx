8237458ee495e40760ae604c5de49bf8e0878708
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-11-12 19:37:55 -0300

    feat: dashboard ok

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </BrowserRouter>
 );
 
